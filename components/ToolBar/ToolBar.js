85effca851388d63c1f7e072ef867f8bc1a1fc15
Angel Rivera <wrathchild4213@gmail.com>
2017-09-19 17:15:04 -0700

    Fixed search bar now has way to clear text

--- components/ToolBar/ToolBar.js
+++ components/ToolBar/ToolBar.js
@@ -5,9 +5,17 @@ import {View, Text, TextInput, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import configs from './configs';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
 
 export default class ToolBar extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            hasText: false,
+        };
+    }
 
     getProps = () => {
         const { 
@@ -20,8 +28,13 @@ export default class ToolBar extends Component {
             onIconTwoPressed,
             onIconThreePressed,
             searchIconColor,
+            xIconColor,
+            iconSizes,
         } = this.props;
 
+        this.iconSizes = iconSizes;
+        this.hasText = this.state.hasText;
+        this.xIconColor = xIconColor;
         this.searchIconColor = searchIconColor;
         this.textInputProps = textInputProps;
 
@@ -37,6 +50,7 @@ export default class ToolBar extends Component {
         this.onIconThreePressed = onIconThreePressed;
     }
 
+
     createPressableIcon = (icon, onPressed) => {
         return (
             <TouchableOpacity 
@@ -50,6 +64,23 @@ export default class ToolBar extends Component {
         );
     }
 
+    displayXIcon = (text) => {
+        (text) ? this.setState({hasText: true}) : 
+            this.setState({hasText: false});
+    }
+
+    xIconPressed = () => {
+        this.textInput.clear();
+        //hack only way to fix otherwise keyboard keeps state
+        this.textInput.setNativeProps({keyboardType: 'email-address'});
+        this.textInput.setNativeProps({keyboardType: 'default'});
+        this.displayXIcon();
+    }
+
+    onChangeText = (text) => {
+        this.displayXIcon(text);
+    }
+
     render () {
         this.getProps();
 
@@ -61,7 +92,15 @@ export default class ToolBar extends Component {
                 <TextInput 
                     {...this.textInputProps}
                     style={[styles.textInput, this.textInputStyle]}
+                    onChangeText={this.onChangeText}
+                    ref={(input) => { this.textInput = input; }}
                 />
+                {
+                    (this.hasText) ? this.createPressableIcon(
+                        <EvilIcon name={'close'} size={28} color={this.xIconColor} />,
+                        this.xIconPressed)
+                        : null
+                }
                 {(this.iconOne) ? this.createPressableIcon(this.iconOne, this.onIconOnePressed): null}
                 {(this.iconTwo) ? this.createPressableIcon(this.iconTwo, this.onIconTwoPressed): null}
                 {(this.iconThree) ? this.createPressableIcon(this.iconThree, this.onIconThreePressed): null}
