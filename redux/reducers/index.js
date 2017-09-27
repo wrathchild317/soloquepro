bfb950cbcb30517891b389893b6f7beaab72c717
stefanagloginic <stefanagloginic@gmail.com>
2017-09-27 16:02:35 -0700

    fixed what was causing crash

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -5,14 +5,12 @@ import AppNavigationReducer from './AppNavigation-reducer';
 import HomeNavigationReducer from './HomeNavigation-reducer';
 import ChampionsNavigationReducer from './ChampionsNavigation-reducer';
 import ChampionReducer from './Champion-reducer';
-import staticDataReducer from './StaticData-reducer';
 
 const allReducers = combineReducers({
     appNavigation: AppNavigationReducer,
     homeNavigation: HomeNavigationReducer,
     championsNavigation: ChampionsNavigationReducer,
     championData: ChampionReducer,
-    staticData: staticDataReducer,
 });
 
 export default allReducers
