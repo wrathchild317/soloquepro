9e9adde41e4b13297928fa02f07e0ed2918f69a6
stefanagloginic <stefana.gloginic@gmail.com>
2017-09-27 22:28:17 -0700

    changed height on click of the info button and changed icon to be smaller

--- components/CarouselCard/CarouselCard.js
+++ components/CarouselCard/CarouselCard.js
@@ -1,8 +1,8 @@
 /*---------------React------------------*/
 import React, { Component } from 'react';
-import {View, Text, Image, TouchableHighlight, Button } from 'react-native';
+import {View, Text, Image, TouchableHighlight, Button, ScrollView, Animated } from 'react-native';
 /*--------------Styles & Configs--------*/
-import styles from './styles';
+import styles, { slideHeight } from './styles';
 import configs from './configs';
 /*-------------import parallax---------*/
 import { ParallaxImage } from 'react-native-snap-carousel';
@@ -17,6 +17,9 @@ export default class CarouselCard extends Component {
     
       this.state = {
         buttonPressed: false,
+        infoBtnPressed: false,
+        heightAnim: new Animated.Value(slideHeight * 0.47),
+        opacityAnim: new Animated.Value(0.7), 
       };
     }
 
@@ -26,6 +29,7 @@ export default class CarouselCard extends Component {
 
     getProps = () => {
         const { champion, parallaxProps} = this.props;
+        const { heightAnim, opacityAnim } = this.state;
         const {champion_id: id, tags, name, title, skinUri} = champion;
         
         /*get champion info for card*/
@@ -40,16 +44,36 @@ export default class CarouselCard extends Component {
 
         /*set up button info*/
         this.buttonPressed = this.state.buttonPressed;
+        this.infoBtnPressed = this.state.infoBtnPressed;
         var lastChar = this.name[this.name.length-1];
         var possesion = (lastChar != 'S') ? `'S` : `'`;
         this.buttonText = this.name + possesion + ' INFO';
+
+        /*animated values*/
+        this.heightAnim = heightAnim;
+        this.opacityAnim = opacityAnim;
         
         /*other*/
         this.parallaxProps = parallaxProps;
     }
 
     handleButtonPress = () => {
-        return true;
+        this.setState({ infoBtnPressed: !this.infoBtnPressed });
+
+        Animated.timing(
+            this.heightAnim, 
+            {
+                toValue: slideHeight * 0.9,
+                duration: 300,
+            }
+        ).start();
+        Animated.timing(
+            this.opacityAnim, 
+            {
+                toValue: 0.875,
+                duration: 300,
+            }
+        ).start();
     }
     onButtonPressIn = () => {
         this.setState({buttonPressed: true});
@@ -75,19 +99,7 @@ export default class CarouselCard extends Component {
     }
 
     get championInfo() {
-        return(
-            <View style={styles.info}>
-                <View style={styles.tagContainer}>
-                    <Text style={styles.tag}>{this.tag}</Text>
-                    <View style={styles.line} />
-                </View>
-                <View style={styles.mainInfoContainer}>
-                    <Image style={styles.tagImage} source={{uri: this.tagUri}} />
-                    <View style={styles.fullNameContainer}>
-                        <Text style={styles.name}>{this.name}</Text>
-                        <Text numberOfLines={2} style={styles.title}>{this.title}</Text> 
-                    </View>
-                </View>
+        var infoBtn = !this.infoBtnPressed ? (
                 <View style={styles.buttonContainer}>
                     <Ripple
                         rippleColor='white'
@@ -103,10 +115,33 @@ export default class CarouselCard extends Component {
                             <Text style={styles.buttonText}>{ this.buttonText }</Text>
                     </Ripple>
                 </View>
+            ) : null 
+
+        return(
+            <View style={styles.info}>
+                <View style={ this.infoBtnPressed ? styles.tagContainerAnimated : styles.tagContainer } >
+                    { !this.infoBtnPressed ? <Text style={styles.tag}>{ this.tag }</Text> : null }
+                </View>
+                <View style={[styles.line, this.infoBtnPressed ? styles.lineAnimated : [] ]} />
+                <View style={styles.mainInfoContainer}>
+                    <Image style={[styles.tagImage, this.infoBtnPressed ? styles.tagImageAnimated : [] ]} source={{uri: this.tagUri}} />
+                    <View style={styles.fullNameContainer}>
+                        <Text style={styles.name}>{this.name}</Text>
+                        <Text numberOfLines={2} style={styles.title}>{ this.title }</Text> 
+                    </View>
+                </View>
+                { infoBtn }
             </View>
         );
     }
 
+    get champDescription() {
+        return(
+            <View style={ {flex: 1} }>
+                
+            </View>
+            )
+    }
 
     render () {
         this.getProps();
@@ -115,9 +150,10 @@ export default class CarouselCard extends Component {
             <View style={styles.slideInnerContainer}>
                 <View style={styles.imageContainer}>
                     { this.image }
-                    <View style={styles.radiusMask} >
+                    <Animated.ScrollView style={[styles.radiusMask, {height: this.heightAnim, opacity: this.opacityAnim}]} >
                         { this.championInfo }
-                    </View>
+                        { this.infoBtnPressed ? this.champDescription : null }
+                    </Animated.ScrollView>
                 </View>
             </View>
         );
