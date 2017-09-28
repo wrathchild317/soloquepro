16030852e6b2b64336f284159df033eca4315725
stefanagloginic <stefana.gloginic@gmail.com>
2017-09-28 01:47:05 -0700

    adjusted animation so that ripple displays onclick of info button, added margin to text so it doesnt go out of card

--- components/CarouselCard/styles.js
+++ components/CarouselCard/styles.js
@@ -64,7 +64,7 @@ export default StyleSheet.create({
         justifyContent: 'flex-start',
         height: 80,
         paddingTop: 5,
-        marginRight: 5,
+        marginRight: 25,
     },
     buttonContainer: {
         width: itemWidth,
